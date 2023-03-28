a17de8054f07489eb7864950d9225e6c9f6fcdad
Peter Iwanowitsch <peter@iwanowitsch.net>
2023-03-28 10:07:02 +0200

    Added german language files

---
+++ files/inc/languages/deutsch_du/cookielaw.lang.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Cookie Law 1.0.0 - Language File
+
+ * Copyright 2023 Peter Iwanowitsch
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ ** http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+**/
+
+$l['cookielaw_header'] = 'Dieses Forum verwendet Cookies';
+$l['cookielaw_description'] = 'Dieses Forum verwendet Cookies, um Deine Anmeldeinformationen zu speichern, wenn Du registriert bist und das Forum besuchst. Cookies sind kleine Textdokumente, die auf Deinem Computer gespeichert werden. Die von diesem Forum gesetzten Cookies können nur auf diesem Forum verwendet werden und stellen kein Sicherheitsrisiko dar. Cookies in diesem Forum verfolgen z.B. welche Themen Du gelesen hast und wann Du sie zuletzt gelesen hast. Bitte bestätige und erlaube damit das Setzen dieser Cookies.';
+$l['cookielaw_description_setcookie'] = 'Unabhängig von Deiner Wahl wird ein Cookie in Deinem Browser gespeichert, um zu verhindern, dass Du diese Frage nicht erneut gestellt bekommst. Du kannst die Cookie-Einstellungen jederzeit über die Fußzeile ändern.';
+$l['cookielaw_description_no_cookies'] = 'Du hast die Verwendung von Cookies auf dieser Website abgelehnt. Einige Funktionen der Website werden somit nicht funktionieren. Du kannst Deine <a href="{1}/misc.php?action=cookielaw_info">Cookie-Einstellung ändern</a> jederzeit ändern.';
+$l['cookielaw_allow'] = 'Cookies erlauben';
+$l['cookielaw_disallow'] = 'Cookies nicht erlauben';
+$l['cookielaw_ok'] = 'Cookies erlauben';
+$l['cookielaw_disallow_confirm'] = "Bist Du sicher, dass Du Cookies nicht zulassen möchtest?'+'\\r\\n\\r\\n'+'Das bedeutet, dass Du dich nicht in das Forum einloggen kannst und dass die Allgemeine Funktionalität des Forums eingeschränkt ist.";
+$l['cookielaw_more_info'] = 'Weitere Informationen';
+$l['cookielaw_info_title'] = 'Cookie-Zustimmung';
+$l['cookielaw_info_cookie_name'] = 'Cookie Name';
+$l['cookielaw_info_cookie_description'] = 'Cookie Beschreibung';
+$l['cookielaw_info_cookies_set_logged_in'] = 'Eingestellt, wenn Du eingeloggt bist';
+$l['cookielaw_info_cookies_set_guest'] = 'Eingestellt, wenn Du Gast bist';
+$l['cookielaw_info_cookies_set_mod'] = 'Moderator';
+$l['cookielaw_info_cookies_set_admin'] = 'Admin';
+$l['cookielaw_info_cookies_set_other'] = 'Andere gesetzte Cookies';
+$l['cookielaw_cookie_sid_desc'] = 'Die ID Deiner Sitzung.';
+$l['cookielaw_cookie_mybbuser_desc'] = 'Deine Benutzer-ID und Dein Anmeldeschlüssel, mit denen Du angemeldet bleibst.';
+$l['cookielaw_cookie_forumpass_desc'] = 'Die Passwörter der passwortgeschützten Foren.';
+$l['cookielaw_cookie_collapsed_desc'] = 'Welche Forumskategorien Du auf der Startseite des Forums ausgeblendet hast.';
+$l['cookielaw_cookie_multiquote_desc'] = 'Welche Beiträge Du als Mehrfachzitate ausgewählt hast.';
+$l['cookielaw_cookie_mybblang_desc'] = 'Welche Sprache Du in der Sprachauswahl gewählt hast.';
+$l['cookielaw_cookie_mybbtheme_desc'] = 'Welches Thema Du in der Themenauswahl gewählt hast.';
+$l['cookielaw_cookie_coppauser_desc'] = 'Ob Du bei der Registrierung ein COPPA-Nutzer bist.';
+$l['cookielaw_cookie_coppadob_desc'] = 'Dein Geburtsdatum, wenn Sie ein COPPA-Nutzer bist.';
+$l['cookielaw_cookie_loginattempts_desc'] = 'Wie oft Du bereits erfolglos versucht hast, Dich anzumelden.';
+$l['cookielaw_cookie_failedlogin_desc'] = 'Wenn Du erfolglos versucht hast, sich anzumelden.';
+$l['cookielaw_cookie_mybbratethread_desc'] = 'Welche Themen Du bewertet hast, um sie nicht erneut bewerten zu können.';
+$l['cookielaw_cookie_mybb[lastvisit]_desc'] = 'Wann Du das Forum zuletzt besucht hast.';
+$l['cookielaw_cookie_mybb[lastactive]_desc'] = 'Wann Du zuletzt im Forum aktiv warst.';
+$l['cookielaw_cookie_mybb[threadread]_desc'] = 'Die von Dir gelesenen Themen.';
+$l['cookielaw_cookie_mybb[forumread]_desc'] = 'Die Foren, die Du als gelesen markiert haben.';
+$l['cookielaw_cookie_mybb[readallforums]_desc'] = 'Als gelesene Foren markiert.';
+$l['cookielaw_cookie_mybb[announcements]_desc'] = 'Welche Ankündigungen Du gelesen hast.';
+$l['cookielaw_cookie_mybb[referrer]_desc'] = 'Die ID des Benutzers, der Dich auf das Forum hingewiesen hat als Du Dich registriert hast.';
+$l['cookielaw_cookie_fcollapse_desc'] = 'Welche Hilfeseiten Du zusammengeklappt hast.';
+$l['cookielaw_cookie_pollvotes_desc'] = 'Verfolgt die Abstimmungsergebnisse.';
+$l['cookielaw_cookie_mybb[allow_cookies]_desc'] = 'Erinnert Dich daran, dass Du die Verwendung von Cookies erlaubt hast. Hinweis: Dieses Cookie wird unabhängig von Deiner Wahl gesetzt, um zu verhindern, dass Du bei jedem Besuch danach erneut gefragt wirst.';
+$l['cookielaw_cookie_inlinemod_*_desc'] = 'Verfolgt, welche Themen/Beiträge für die Inline-Moderation ausgewählt wurden.';
+$l['cookielaw_cookie_adminsid_desc'] = 'Hält Dich im Admin-Kontrollzentrum angemeldet.';
+$l['cookielaw_cookie_acploginattempts_desc'] = 'Wie oft Du schon erfolglos versucht hast, dich in das Admin-Kontrollzentrum einzuloggen.';
+$l['cookielaw_cookie_acpview_desc'] = 'Anzeige der Benutzeransicht im Admin Control Panel.';
+$l['cookielaw_redirect'] = 'Deine Cookie-Präferenzen wurden aktualisiert.';
+$l['cookielaw_footer'] = 'Einstellungen für die Cookie-Zustimmung';
+?>
